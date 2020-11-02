6e927c208422dcc5035a7a0fedf4a6dea337c654
Vince250598 <vincentgagnon53@gmail.com>
2020-11-02 11:26:59 -0500

    connection error

--- SQL_DEPART/scripts/declencheurs/nouveauSuccesEnCours.sql
+++ SQL_DEPART/scripts/declencheurs/nouveauSuccesEnCours.sql
@@ -3,18 +3,18 @@ BEFORE INSERT
 ON SUCCES_EN_COURS
 FOR EACH ROW
 
-DECLARE COMPLETED_SUCCESS number:=0;
+--DECLARE COMPLETED_SUCCESS number:=0;
 
 BEGIN
 
-    SELECT count(*) INTO COMPLETED_SUCCESS from SUCCES_REALISE where IdSucces = :NEW.IDSUCCES;
-    
+    --SELECT count(*) INTO COMPLETED_SUCCESS from SUCCES_REALISE where IdSucces = :NEW.IDSUCCES;
+    --DBMS_OUTPUT.PUT_LINE('success: ' || COMPLETED_SUCCESS);
     --if we find a already existing success, don't insert
-    IF (COMPLETED_SUCCESS > 0)
-    THEN
-     raise_application_error(-20000, 'Succès déjà complété.');
-    END IF;
-    
+    --IF (COMPLETED_SUCCESS > 0)
+    --THEN
+    -- raise_application_error(-20000, 'Succès déjà complété.');
+    --END IF;
+    null;
     
 
 END;
