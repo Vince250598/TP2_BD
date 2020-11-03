ba5712be4347d62a2eae4afbe5c5dfe068dfaaf5
Vince250598 <vincentgagnon53@gmail.com>
2020-11-03 14:54:27 -0500

    finished procedures

--- SQL_DEPART/scripts/declencheurs/nouveauSuccesEnCours.sql
+++ SQL_DEPART/scripts/declencheurs/nouveauSuccesEnCours.sql
@@ -3,17 +3,16 @@ BEFORE INSERT
 ON SUCCES_EN_COURS
 FOR EACH ROW
 
---DECLARE COMPLETED_SUCCESS number:=0;
+DECLARE COMPLETED_SUCCESS number:=0;
 
 BEGIN
 
-    --SELECT count(*) INTO COMPLETED_SUCCESS from SUCCES_REALISE where IdSucces = :NEW.IDSUCCES;
-    --DBMS_OUTPUT.PUT_LINE('success: ' || COMPLETED_SUCCESS);
+    SELECT count(*) INTO COMPLETED_SUCCESS from SUCCES_REALISE where IdSucces = :NEW.IDSUCCES;
     --if we find a already existing success, don't insert
-    --IF (COMPLETED_SUCCESS > 0)
-    --THEN
-    -- raise_application_error(-20000, 'Succès déjà complété.');
-    --END IF;
+    IF (COMPLETED_SUCCESS > 0)
+    THEN
+     raise_application_error(-20000, 'Succès déjà complété.');
+    END IF;
     null;
     
 
