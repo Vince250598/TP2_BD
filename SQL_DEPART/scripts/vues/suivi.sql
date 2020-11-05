4ccda63f61e757549b91d7781b2b183b76070d40
Hans Bélanger <hans.belanger@pm.me>
2020-11-05 11:53:09 -0500

    finished vues

--- SQL_DEPART/scripts/vues/suivi.sql
+++ SQL_DEPART/scripts/vues/suivi.sql
@@ -5,8 +5,14 @@ where suivi_reseau.nojoueur is null
 order by suivi_reseau.moment;/
 
 
-CREATE OR REPLACE VIEW SuiviReseayJoueur
+CREATE OR REPLACE VIEW SuiviReseauJoueur
 as select suivi_reseau.moment, suivi_reseau.action, suivi_reseau.codereseau, joueur.surnom  from suivi_reseau
 join joueur on suivi_reseau.nojoueur = joueur.nojoueur 
 where suivi_reseau.nojoueur is not null
 order by suivi_reseau.moment;/ 
+
+
+CREATE OR REPLACE VIEW SuiviAmitieInvitation
+as select suivi_amitie.moment, joueur.surnom, joueur.nojoueur, suivi_amitie.nojoueurinvite, suivi_amitie.datesuivi, suivi_amitie.codestatut from SUIVI_AMITIE
+join joueur on joueur.nojoueur = suivi_amitie.nojoueurinvitant
+order by Suivi_amitie.moment;/
